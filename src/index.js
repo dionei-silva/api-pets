84049e0ed187660f6ad2b193a70fcc481655d7cb
Dionei Silva <dionei.silva@gmail.com>
2025-10-01 19:52:12 -0300

    feat: add middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import {randomUUID} from 'crypto'
+import { logMiddleware } from './middleware.js'
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+app.use(logMiddleware)
 
 app.get('/pets', (req, res) => {
   res.send('Hello World!')
