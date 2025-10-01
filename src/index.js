9b857a323fed9a9bd78461282c8486de3a877551
Dionei Silva <dionei.silva@gmail.com>
2025-10-01 20:18:44 -0300

    feat: add get pets

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as dotenv from 'dotenv'
 import {randomUUID} from 'crypto'
 import { logMiddleware } from './middleware.js'
 
+import {pets} from './pets.js'
+
 dotenv.config()
 
 const app = express()
@@ -10,9 +12,24 @@ const app = express()
 app.use(express.json())
 app.use(logMiddleware)
 
+//GET
 app.get('/pets', (req, res) => {
-  res.send('Hello World!')
+  let data = pets
+  try {
+    res.status(200).send({
+      ok: true,
+      message: "Pets listados com sucesso",
+      data
+    })
+    
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      Message: error.toString()
+    })
+  }
 })
+
 
 
 const port = process.env.PORT
