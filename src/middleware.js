b6654d31c30265043d6524a2111de3c50e8b2fd8
Dionei Silva <dionei.silva@gmail.com>
2025-10-06 20:26:19 -0300

    feat: adicionado middleware de validação de campos do usuario

--- src/middleware.js
+++ src/middleware.js
@@ -25,3 +25,42 @@ export const checkPetIdMiddleware = (req, res, next) => {
             })
       }
 }
+
+export const validatePetMiddleware = (req, res, next) => {
+      try {
+            const {nome, idade, raca, nomeTutor} = req.body
+
+            if (!nome) {
+                  return res.status(400).send({
+                        ok:false,
+                        mensagem: "O campo nome nao foi informado"
+                  })                  
+            }
+            if (!raca) {
+                  return res.status(400).send({
+                        ok:false,
+                        mensagem: "O campo raca nao foi informado"
+                  })                  
+            }
+            if (!idade) {
+                  return res.status(400).send({
+                        ok:false,
+                        mensagem: "O campo idade nao foi informado"
+                  })               
+            }
+            if (!nomeTutor) {
+                  return res.status(400).send({
+                        ok:false,
+                        mensagem: "O campo nome do tutor nao foi informado"
+                  }) 
+            }  
+            next()
+      } catch (error) {
+            console.log(error);
+            return res.status(500).send({
+                  ok:false,
+                  mensage: error.toString()
+            })
+            
+      }
+}
